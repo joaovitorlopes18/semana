2be8dc640e43d954d291fd2860e90bd763f794bc
João  Vitor <134068768+joaovitorlopes18@users.noreply.github.com>
2023-09-05 12:23:20 -0300

    Add files via upload
    05 09 23

---
+++ semana.js
@@ -0,0 +1,28 @@
+
+let day = 3; // 1 representa Domingo, 2 representa Segunda, e assim por diante
+
+switch (day) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-feira");
+        break;
+    case 3:
+        console.log("Terça-feira");
+        break;
+    case 4:
+        console.log("Quarta-feira");
+        break;
+    case 5:
+        console.log("Quinta-feira");
+        break;
+    case 6:
+        console.log("Sexta-feira");
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("Dia inválido");
+}
